fb9739d679bcf1765d8575894e032ddd1a8ed96b
pramit-marattha <pramitmarattha5497@gmail.com>
2021-11-01 00:55:50 +0545

    note and noteadd component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import firebase from "firebase/app";
+import Navbar from "./components/Navbar";
+import NoteAdd from "./components/NoteAdd";
 
-function App() {
+import "./App.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1qxiarwXOltSqNp345h5FxiJgPQF4Fx4",
+  authDomain: "react-firebase-notebook-19187.firebaseapp.com",
+  databaseURL:
+    "https://react-firebase-notebook-19187-default-rtdb.firebaseio.com",
+  projectId: "react-firebase-notebook-19187",
+  storageBucket: "react-firebase-notebook-19187.appspot.com",
+  messagingSenderId: "819670459741",
+  appId: "1:819670459741:web:f40f70203b267e5c6e74b4",
+  measurementId: "G-KVBJP9RYB0",
+};
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <div className="note-section">
+        <NoteAdd />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
